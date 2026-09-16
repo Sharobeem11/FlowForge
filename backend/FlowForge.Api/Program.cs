a922575da7f2f2ab9376f46334e6f0fed1359b61
Michael <sharobeem11@hotmail.com>
2026-09-16 15:09:00 -0400

    create backend web api

---
+++ backend/FlowForge.Api/Program.cs
@@ -0,0 +1,16 @@
+using FlowForge.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+app.MapGet("/", () => {
+    return "FlowForge API is running!";
+});
+
+app.Run();
